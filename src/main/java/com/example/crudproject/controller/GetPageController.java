ef6eb8aaf66514d2e355f220a628598aca9c40f4
JeonDaehong <daehong770@gmail.com>
2023-11-03 16:05:07 +0900

    feat : Board(게시판) 기능 추가 및 주석 달기

---
+++ src/main/java/com/example/crudproject/controller/GetPageController.java
@@ -0,0 +1,180 @@
+package com.example.crudproject.controller;
+
+import com.example.crudproject.domain.Board;
+import com.example.crudproject.domain.User;
+import com.example.crudproject.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class GetPageController {
+
+    private final BoardService boardService;
+
+    /**
+     * 메인 페이지 이동
+     */
+    @GetMapping ("/")
+    public String main(HttpSession session, Model model) {
+
+        User user = (User) session.getAttribute("loginUser");
+        boolean loginSession = user != null;
+        model.addAttribute("user", user);
+        model.addAttribute("loginSession", loginSession);
+
+        return "/main";
+    }
+
+    /**
+     * 로그인 페이지 이동
+     */
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "/login";
+    }
+
+    /**
+     * 회원가입 페이지 이동
+     */
+    @GetMapping("/joinPage")
+    public String joinPage() {
+        return "/join";
+    }
+
+    /**
+     * 내 정보 페이지로 이동
+     */
+    @GetMapping("/userInfoPage")
+    public String userInfo(HttpSession session, Model model) {
+
+        // Session 끊킬 시 redirect
+        User user = (User) session.getAttribute("loginUser");
+        boolean loginSession = user != null;
+        if ( !loginSession ) return "redirect:/loginPage";
+        model.addAttribute("user", user);
+        model.addAttribute("loginSession", loginSession);
+
+        return "/userInfo";
+    }
+
+    /**
+     * 게시판 리스트 페이지로 이동
+     *  ++ 페이징 처리 10개씩
+     */
+    @GetMapping("/boardListPage")
+    public String boardListPage(HttpSession session, Model model,
+                                @RequestParam(value = "startIdx", defaultValue = "0") int startIdx,
+                                @RequestParam(value = "page", defaultValue = "1") int page,
+                                @RequestParam(value = "pagingType", defaultValue = "0") int pagingType) {
+
+        // Session 끊킬 시 redirect
+        User user = (User) session.getAttribute("loginUser");
+        boolean loginSession = user != null;
+        if ( !loginSession ) return "redirect:/loginPage";
+        model.addAttribute("user", user);
+        model.addAttribute("loginSession", loginSession);
+
+        // 페이징 처리
+        boolean nextPage = false;
+        boolean beforePage = false;
+        if ( pagingType == 1 ) { // 다음
+            startIdx += 10;
+            page ++;
+        } else if ( pagingType == 2 ) { // 이전
+            startIdx -= 10;
+            page --;
+        }
+        if ( page > 1 ) beforePage = true;
+
+        int totalPageCount = boardService.getBoardCount();
+        if ( totalPageCount > startIdx + 10 ) nextPage = true;
+
+        int count = 10;
+        List<Board> boardList = boardService.getBoardList(startIdx, count);
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startIdx", startIdx);
+        model.addAttribute("page", page);
+        model.addAttribute("nextPage", nextPage);
+        model.addAttribute("beforePage", beforePage);
+
+        return "/boardList";
+    }
+
+    /**
+     * 게시글 작성 페이지로 이동
+     */
+    @GetMapping("/boardWritePage")
+    public String boardWritePage(HttpSession session, Model model) {
+
+        // Session 끊킬 시 redirect
+        User user = (User) session.getAttribute("loginUser");
+        boolean loginSession = user != null;
+        if ( !loginSession ) return "redirect:/loginPage";
+
+        model.addAttribute("user", user);
+        model.addAttribute("loginSession", loginSession);
+
+        return "/boardWrite";
+    }
+
+
+    /**
+     * 게시글 상세 정보 페이지로 이동
+     */
+    @GetMapping("/boardInfoPage")
+    public String boardInfoPage(HttpSession session, Model model,
+                                @RequestParam(value = "boardId", required = false) Long boardId,
+                                @RequestParam(value = "userId", required = false) Long userId ) {
+
+        // Session 끊킬 시 redirect
+        User user = (User) session.getAttribute("loginUser");
+        boolean loginSession = user != null;
+        if ( !loginSession ) return "redirect:/loginPage";
+
+        model.addAttribute("user", user);
+        model.addAttribute("loginSession", loginSession);
+
+        Board board = boardService.getBoardInfo(boardId);
+        model.addAttribute("board", board);
+
+        // 수정, 삭제 버튼 활성화를 위한 boolean
+        boolean fixPossible = false;
+        if (Objects.equals(user.getUserId(), userId)) fixPossible = true;
+        model.addAttribute("fixPossible", fixPossible);
+
+        return "/boardInfo";
+    }
+
+    /**
+     * 게시글 수정 페이지로 이동
+     */
+    @GetMapping("/boardUpdatePage")
+    public String boardUpdatePage(HttpSession session, Model model,
+                                  @RequestParam(value = "boardId", required = false) Long boardId) {
+
+        // Session 끊킬 시 redirect
+        User user = (User) session.getAttribute("loginUser");
+        boolean loginSession = user != null;
+        if ( !loginSession ) return "redirect:/loginPage";
+
+        model.addAttribute("user", user);
+        model.addAttribute("loginSession", loginSession);
+
+        Board board = boardService.getBoardInfo(boardId);
+        model.addAttribute("board", board);
+
+        return "/boardUpdate";
+    }
+
+}
