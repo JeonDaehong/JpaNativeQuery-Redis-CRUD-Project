872f4a793dfabe751a11c4fd1ebe312d1ead6dba
JeonDaehong <daehong770@gmail.com>
2023-11-13 19:40:43 +0900

    feature : Redis 추가

--- src/main/java/com/example/crudproject/controller/BoardController.java
+++ src/main/java/com/example/crudproject/controller/BoardController.java
@@ -6,9 +6,7 @@ import com.example.crudproject.domain.User;
 import com.example.crudproject.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
