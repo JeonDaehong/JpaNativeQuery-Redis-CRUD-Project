1cf1932104072b27423d65296739de35548fdbba
JeonDaehong <daehong770@gmail.com>
2023-11-28 00:18:15 +0900

    feature : 평점 관련 스케줄러 추가

--- src/main/java/com/example/crudproject/common/batch/RedisBatch.java
+++ src/main/java/com/example/crudproject/common/batch/RedisBatch.java
@@ -1,6 +1,7 @@
 package com.example.crudproject.common.batch;
 
 import com.example.crudproject.domain.board.service.BoardService;
+import com.example.crudproject.domain.board.service.ScoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,11 +11,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RedisBatch {
 
-    private final RedisTemplate<String, Integer> redisTemplate;
     private final BoardService boardService;
+    private final ScoreService scoreService;
 
-    @Scheduled(fixedDelay = 1000 * 60 * 60) // 1시간(60분)에 한 번 실행
+    @Scheduled(fixedDelay = 1000 * 60) // 1분에 한 번 실행
     public void transferBoardViewRedisToDB() {
         boardService.transferBoardViewRedisToDB();
+    }
+
+    @Scheduled(fixedDelay = 1000 * 60) // 1분에 한 번 실행
+    public void insertAverageScoreDB() {
+        scoreService.calculateRating();
     }
 }
